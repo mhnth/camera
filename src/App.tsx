9a6a7144ee33d7f9ec7140f4b33e0563b66d221a
manhnth <n.t.manh@viscirclevn.com>
2024-02-23 16:49:22 +0700

    u

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useRef, useState } from 'react';
 const FaceDetectionComponent: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const virtualCanvasRef = useRef<HTMLCanvasElement>(null);
   const [isCameraOn, setIsCameraOn] = useState<boolean>(false);
   const [cutImage, setCutImage] = useState<string | null>(null);
 
@@ -35,36 +36,105 @@ const FaceDetectionComponent: React.FC = () => {
       }
     };
 
-    if (isCameraOn && videoRef.current && canvasRef.current) {
+    if (
+      isCameraOn &&
+      videoRef.current &&
+      canvasRef.current &&
+      virtualCanvasRef.current
+    ) {
       startCamera();
       const video = videoRef.current;
+      const virtualCanvas = virtualCanvasRef.current;
       const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
+      const virtualContext = virtualCanvas!.getContext('2d');
+      const context = canvas!.getContext('2d');
 
-      // navigator.mediaDevices
-      //   .getUserMedia({ video: true })
-      //   .then(function (stream) {
-      //     video.srcObject = stream;
-      //     console.log('hello');
-      //   })
-      //   .catch(function (err) {
-      //     console.error('Error accessing the camera:', err);
-      //   });
+      const cutImageIntoEllipseVirtual = () => {
+        if (!video.paused && !video.ended && virtualContext) {
+          // Clear the canvas
+          virtualContext.clearRect(
+            0,
+            0,
+            virtualCanvas.width,
+            virtualCanvas.height
+          );
+
+          // Draw the video frame on the canvas
+          virtualContext.drawImage(
+            video,
+            0,
+            0,
+            virtualCanvas.width,
+            virtualCanvas.height
+          );
+
+          // Create a background rectangle with a solid color
+          virtualContext.fillStyle = 'rgba(255, 255, 255, 0)'; // You can change the color here
+          virtualContext.fillRect(
+            0,
+            0,
+            virtualCanvas.width,
+            virtualCanvas.height
+          );
+
+          // Create an elliptical path (vertical ellipse)
+          virtualContext.beginPath();
+          virtualContext.ellipse(
+            virtualCanvas.width / 2, // X coordinate of the center
+            virtualCanvas.height / 2, // Y coordinate of the center
+            virtualCanvas.width / 4, // Horizontal radius
+            virtualCanvas.height / 2.95, // Vertical radius (adjust as needed)
+            0, // Rotation angle (0 for vertical)
+            0, // Start angle
+            Math.PI * 2 // End angle
+          );
+          virtualContext.closePath();
+
+          // Set the stroke style to white and increase the line width
+          virtualContext.strokeStyle = 'white';
+          virtualContext.lineWidth = 8; // Increase the line width as needed
+
+          // Set the line dash style
+          virtualContext.setLineDash([10, 5]); // [dashLength, spaceLength]
+
+          // Draw the stroke (border) of the ellipse
+          virtualContext.stroke();
+
+          // Clip the canvas to the elliptical path
+          virtualContext.clip();
+
+          // Redraw the video frame, clipped to the elliptical path
+          virtualContext.drawImage(
+            video,
+            0,
+            0,
+            virtualCanvas.width,
+            virtualCanvas.height
+          );
+
+          // context.globalCompositeOperation = 'source-over';
+        }
+
+        // Call recursively to keep updating the frame
+        requestAnimationFrame(cutImageIntoEllipseVirtual);
+      };
 
       const cutImageIntoEllipse = () => {
         if (!video.paused && !video.ended && context) {
           // Clear the canvas
           context.clearRect(0, 0, canvas.width, canvas.height);
 
+          // Set globalCompositeOperation to 'destination-out'
+          context.globalCompositeOperation = 'destination-out';
+
           // Draw the video frame on the canvas
           context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-          // Create a background rectangle with a solid color
-          context.fillStyle = 'white'; // You can change the color here
-          context.fillRect(0, 0, canvas.width, canvas.height);
+          context.globalCompositeOperation = 'source-over';
 
-          // Set globalCompositeOperation to 'destination-out'
-          // context.globalCompositeOperation = 'destination-out';
+          // Create a background rectangle with a solid color
+          context.fillStyle = 'rgba(255, 255, 255, 0)'; // You can change the color here
+          context.fillRect(0, 0, canvas.width, canvas.height);
 
           // Create an elliptical path (vertical ellipse)
           context.beginPath();
@@ -80,8 +150,8 @@ const FaceDetectionComponent: React.FC = () => {
           context.closePath();
 
           // Set the stroke style to white and increase the line width
-          context.strokeStyle = 'red';
-          context.lineWidth = 8; // Increase the line width as needed
+          context.strokeStyle = 'white';
+          context.lineWidth = 2; // Increase the line width as needed
 
           // Set the line dash style
           context.setLineDash([10, 5]); // [dashLength, spaceLength]
@@ -95,7 +165,7 @@ const FaceDetectionComponent: React.FC = () => {
           // Redraw the video frame, clipped to the elliptical path
           context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-          context.globalCompositeOperation = 'source-over';
+          // context.globalCompositeOperation = 'source-over';
         }
 
         // Call recursively to keep updating the frame
@@ -103,6 +173,7 @@ const FaceDetectionComponent: React.FC = () => {
       };
 
       cutImageIntoEllipse();
+      cutImageIntoEllipseVirtual();
     } else {
       stopCamera();
     }
@@ -137,7 +208,15 @@ const FaceDetectionComponent: React.FC = () => {
 
   return (
     <div>
-      <h1>Face Detection with OpenC</h1>
+      <div className="header">
+        <div>Face Detection with OpenC</div>
+        <button onClick={toggleCamera}>
+          {isCameraOn ? 'Turn Off Camera' : 'Turn On Camera'}
+        </button>
+        <button onClick={cutAndDisplayImage}>Cut and Display Image</button>
+        <button onClick={exportImage}>Export Image</button>
+      </div>
+
       <video
         ref={videoRef}
         width={300}
@@ -145,21 +224,29 @@ const FaceDetectionComponent: React.FC = () => {
         style={{ display: isCameraOn ? 'none' : 'none' }}
         autoPlay
       ></video>
-      <canvas
-        ref={canvasRef}
-        width={480}
-        height={600}
-        style={{
-          display: isCameraOn ? 'block' : 'none',
-          backgroundColor: 'transparent',
-        }}
-      ></canvas>
-      <button onClick={toggleCamera}>
-        {isCameraOn ? 'Turn Off Camera' : 'Turn On Camera'}
-      </button>
-      <button onClick={cutAndDisplayImage}>Cut and Display Image</button>
-      <button onClick={exportImage}>Export Image</button>
-      {cutImage && <img src={cutImage} alt="Cut Image" />}
+      <div className="container">
+        <canvas
+          ref={virtualCanvasRef}
+          width={480}
+          height={600}
+          style={{
+            display: isCameraOn ? 'block' : 'none',
+          }}
+          className="canvas1"
+        ></canvas>
+        <canvas
+          ref={canvasRef}
+          width={480}
+          height={600}
+          style={{
+            display: isCameraOn ? 'none' : 'none',
+          }}
+          className="canvas1"
+        ></canvas>
+        <div className="imgBox">
+          {cutImage && <img src={cutImage} alt="Cut Image" />}
+        </div>
+      </div>
     </div>
   );
 };
